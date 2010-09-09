f607985ddf1f7168ec9b10274e327c25afb99280
Sridhar Ratnakumar <me@srid.name>
2010-09-08 18:27:59 -0700

    add minimal test and tox files

---
+++ test.py
@@ -0,0 +1,10 @@
+# TODO: write actual tests
+
+def test_import():
+    import applib
+    import applib.base
+    import applib.sh
+    import applib.textui
+    import applib.log
+    import applib.misc
+
