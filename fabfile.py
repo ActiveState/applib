80cc59294f20ac87c455f4a486bcfa0aed0b3bff
Sridhar Ratnakumar <me@srid.name>
2010-10-06 09:58:03 -0700

    fab: add a test command

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from os import path
 from fabric.api import *
 
@@ -10,3 +11,14 @@ import venv
 
 clean = venv.clean
 init = venv.init
+
+
+def test():
+    venv.install('py')
+    py_test = venv.get_script('py.test')
+    test_script = path.join('applib', 'test', 'all.py')
+    if not path.exists('tmp'):
+        os.mkdir('tmp')
+    local(
+        '{0} -x -v {1} --junitxml=tmp/testreport.xml'.format(py_test, test_script),
+        capture=False)
